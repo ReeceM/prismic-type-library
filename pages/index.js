d09dbb4ac0db021086e9dcdc5e4e08dab330eb51
ReeceM <reecestevenmay@gmail.com>
2021-03-18 01:45:37 +0200

    main page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,69 @@
-import Nav from '../components/nav'
+import Layout from '../components/Layout';
+import Head from 'next/head';
+import Banner from '../components/Banner'
+import { CenteredHero } from '../components/Hero'
+import TypesTable from '../components/TypesTable';
+import { useEffect, useState } from 'react';
+export default function IndexPage({ links }) {
 
-export default function IndexPage() {
+  const [types, setTypes] = useState([])
+
+  useEffect(() => {
+
+    fetch('index.json')
+      .then(res => res.json())
+      .then((res) => {
+        setTypes(res.data);
+      })
+      .catch(e => {
+        console.error(e)
+      })
+
+    return () => { }
+  }, [])
+
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
-    </div>
+    <>
+      <Head>
+        <title>(Unofficial) Prismic Type Library</title>
+      </Head>
+      <Layout menu={{ links: links }}>
+
+        <Banner slice={{
+          shortMessage: "hello",
+          longMessage: 'This is the description',
+          link: '#',
+          linkText: 'The Link'
+        }}
+        />
+        <CenteredHero
+          slice={{
+            eyebrow: "Prismic Type Library",
+            title: "Hello to an Unofficial Prismic Types Library",
+            description: "This is a repo of sorts for a collection of Prismic Custom Types for various aspects of a CMS",
+          }}
+        />
+
+        <main className="pb-20">
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8" >
+            <TypesTable types={types} />
+          </div>
+        </main>
+      </Layout>
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const links = [
+    { href: '/', label: 'Custom Types' },
+    { href: 'https://github.com/reecem/prismic-type-library/pr/new', label: 'Submit a Type' },
+    // { href: '/about', label: 'About' },
+    { href: 'https://github.com/reecem/prismic-type-library', label: 'Readme' },
+    { href: 'https://prismic.io', label: 'Prismic CMS' },
+  ];
+
+  return {
+    props: { links },
+  }
 }
